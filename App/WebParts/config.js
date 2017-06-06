67af8d6a6fbe8f34945d4bcb9094e4be649160bb
Dhruv Bhavsar <dhruvhbhavsar@gmail.com>
2017-06-06 19:23:14 -0400

    Remove unnessary code

--- App/WebParts/config.js
+++ App/WebParts/config.js
@@ -1,55 +1,6 @@
 ﻿var webparts = [
         //Add comments anywhere inside file
         {
-            "controllerName": "directiveSearch",
-            "description": "Find a Directive - Autocompleteing WebPart For Searching Directives ",
-            "include": { "Contoso": false },
-            "title": "Find A Directive",
-            "webpartConfig": { "id": "123124", "data-test": "testParameter", "class": "dhruv's Css Class" }
-        },
-        {
-            "controllerName": "formsSearch",
-            "description": "Find a Form - Autocompleteing WebPart For Searching Forms",
-            "include": { "Contoso": false },
-            "title": "Find A Form",
-            "webpartConfig": {}
-        },
-        {
-            "controllerName": "generalSearch",
-            "description": "This is the typeahead used when searching for general messages",
-            "include": { "Contoso": false },
-            "title": "Find A Coast Guard General Message",
-            "webpartConfig": {}
-        },
-        {
-            "controllerName": "alcoast",
-            "description": "DO NOT USE THIS WEBPART",
-            "include": { "Contoso": false },
-            "title": "Alcoast new items",
-            "webpartConfig": { }
-        },
-        {
-            "controllerName": "exampleList",
-            "description": "Example List Demo",
-            "include": { "Contoso": true },
-            "title": "Demo 1 Webpart",
-            "webpartConfig": { }
-        },
-        {
-            "controllerName": "exampleListGroup",
-            "description": "Example List Group Demo",
-            "include": { "Contoso": true },
-            "title": "Demo 2 Webpart",
-            "webpartConfig": {}
-        },
-        {
-            "controllerName": "exampleOwlDemo",
-            "description": "Example Owl Demo",
-            "include": { "Contoso": true },
-            "title": "Demo 3 Webpart",
-            "webpartConfig": {}
-        },
-        {
             "controllerName": "testWebPart",
             "description": "DO NOT USE THIS WEBPART",
             "include": { "Contoso": false },
